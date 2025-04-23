564403c0f64ac182b5c902131d473e9cd0e97421
bd-SrinathAkkem <reddyakkem@blackduck.com>
2025-04-23 16:50:51 +0530

    Update main.tsx with various mistakes

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,16 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  <SR></SR>
+  
 )
+
+function MyComponent() {
+  return (
+    <div class="container">
+      <h1>Hello World</h1>
+      <button onclick="alert('Clicked!')">Click Me</button>
+    </div>
+  )
+}
+
+export default MyComponent
