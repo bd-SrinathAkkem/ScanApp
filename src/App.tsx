e4a4f2c479dd76864114a61ff2f3aca27d613e71
Srinath Akkem <reddyakkem@blackduck.com>
2025-06-02 16:55:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import ListGroup from "./components/ListGroup" // we need this one this time
 
 function App() {
-  const abd = ''
-  abd = 123
-  return <div><ListGroup/></diveded for it!
+  return <div><ListGroup /></div>
 }
 
 export default App // for it to be used somewhere else
